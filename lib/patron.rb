492c90b78c89fb3663a22b7d41d3f5e1733b8a28
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-19 09:16:59 -0700

    Add test_it_can_add_interests to patron test

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,7 +1,9 @@
 class Patron
+  attr_reader :name, :spending_money, :interests
 
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
+    @interests = []
   end
 end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -16,4 +16,12 @@ class PatronTest < Minitest::Test
     assert_equal 20, @patron_1.spending_money
     assert_equal [], @patron_1.interests
   end
+
+  def test_it_can_add_interests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    assert_equal ["Dead Sea Scrolls"], @patron_1.interests
+
+    @patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
+  end
 end
