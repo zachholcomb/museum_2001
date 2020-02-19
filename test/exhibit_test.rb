5f40d13747b10cbac6b4c8e395688304f7340398
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-19 09:05:46 -0700

    Add test_instance_of to exhibit test

---
+++ test/exhibit_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+
+class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+end
+
+
+# pry(main)> exhibit.name
+# # => "Gems and Minerals"
+#
+# pry(main)> exhibit.cost
+# # => 0
+#
+# pry(main)> patron_1 = Patron.new("Bob", 20)
+# # => #<Patron:0x00007fcb13b5c7d8...>
+#
+# pry(main)> patron_1.name
+# # => "Bob"
+#
+# pry(main)> patron_1.spending_money
+# # => 20
+#
+# pry(main)> patron_1.interests
+# # => []
+#
+# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> patron_1.add_interest("Gems and Minerals")
+#
+# pry(main)> patron_1.interests
+# # => ["Dead Sea Scrolls", "Gems and Minerals"]
