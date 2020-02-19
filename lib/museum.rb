677298348073d2be29eaad30dfdf2384314d22e0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-19 09:32:38 -0700

    Add test_it_can_recomend_exhibits to Museum test

--- lib/museum.rb
+++ lib/museum.rb
@@ -5,4 +5,8 @@ class Museum
     @name = name
     @exhibits = []
   end
+
+  def add_exhibit(new_exhibit)
+    @exhibits << new_exhibit
+  end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -10,6 +10,10 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_2.add_interest("IMAX")
   end
 
   def test_it_exists
@@ -31,27 +35,18 @@ class MuseumTest < Minitest::Test
   @dmns.add_exhibit(@imax)
   assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
+
+  def test_it_can_recomend_exhibits
+    recommended_exhibits1 = [@gems_and_minerals, @dead_sea_scrolls]
+    assert_equal recommended_exhibits1, @dmns.recommend_exhibits(@patron_1)
+
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
 end
 
 
 
 
-
-# pry(main)> dmns.exhibits
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>, #<Exhibit:0x00007fb400acc590...>]
-#
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fb400a51cc8...>
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_2 = Patron.new("Sally", 20)
-# # => #<Patron:0x00007fb400036338...>
-#
-# pry(main)> patron_2.add_interest("IMAX")
-#
 # pry(main)> dmns.recommend_exhibits(patron_1)
 # # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
 #
