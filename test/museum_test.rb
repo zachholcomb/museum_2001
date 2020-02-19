8a3b2704e1dcd6e4b9eea8acd78510d51d9ca6ee
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-19 09:55:15 -0700

    Add patrons assertion to attributes test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -24,6 +24,7 @@ class MuseumTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
   end
 
   def test_it_can_add_exhibits
@@ -47,13 +48,67 @@ class MuseumTest < Minitest::Test
 
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
+
 end
 
 
-
-
-# pry(main)> dmns.recommend_exhibits(patron_1)
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
 #
-# pry(main)> dmns.recommend_exhibits(patron_2)
-# # => [#<Exhibit:0x00007fb400acc590...>]
+# pry(main)> dmns.patrons
+# # => []
+#
+# pry(main)> patron_1 = Patron.new("Bob", 0)
+# # => #<Patron:0x00007fb2011455b8...>
+#
+# pry(main)> patron_1.add_interest("Gems and Minerals")
+#
+# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> patron_2 = Patron.new("Sally", 20)
+# # => #<Patron:0x00007fb20227f8b0...>
+#
+# pry(main)> patron_2.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> patron_3 = Patron.new("Johnny", 5)
+# # => #<Patron:0x6666fb20114megan...>
+#
+# pry(main)> patron_3.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> dmns.admit(patron_1)
+#
+# pry(main)> dmns.admit(patron_2)
+#
+# pry(main)> dmns.admit(patron_3)
+#
+# pry(main)> dmns.patrons
+# # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>]
+#
+# #Patrons are added even if they don't have enough money for all/any exhibits.
+#
+# pry(main)> dmns.patrons_by_exhibit_interest
+# # =>
+# # {
+# #   #<Exhibit:0x00007fb202238618...> => [#<Patron:0x00007fb2011455b8...>],
+# #   #<Exhibit:0x00007fb202248748...> => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>],
+# #   #<Exhibit:0x00007fb20225f8d0...> => []
+# # }
+#
+# pry(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
+# # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x6666fb20114megan...>]
+#
+# pry(main)> dmns.draw_lottery_winner(dead_sea_scrolls)
+# # => "Johnny" or "Bob" can be returned here. Fun!
+#
+# pry(main)> dmns.draw_lottery_winner(gems_and_minerals)
+# # => nil
+#
+# #If no contestants are elgible for the lottery, nil is returned.
+#
+# pry(main)> dmns.announce_lottery_winner(dead_sea_scrolls)
+# # => "Bob has won the Dead Sea Scrolls exhibit lottery"
+#
+# # The above string should match exactly, you will need to stub the return of `draw_lottery_winner` as the above method should depend on the return value of `draw_lottery_winner`.
+#
+# pry(main)> dmns.announce_lottery_winner(gems_and_minerals)
+# # => "No winners for this lottery"
+#
+# # If there are no contestants, there are no winners.
