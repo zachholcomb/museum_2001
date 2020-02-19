97b6231f86964ceaa79959020aefff7c0ad45404
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-19 09:08:02 -0700

    Add test_it_has_attributes to exhibit test

---
+++ lib/exhibit.rb
@@ -0,0 +1,6 @@
+class Exhibit
+
+  def initialize(argument)
+    @argument = argument
+  end
+end
--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -10,6 +10,11 @@ class ExhibitTest < Minitest::Test
   def test_it_exists
     assert_instance_of Exhibit, @exhibit
   end
+
+  def test_it_has_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
 end
 
 
