2d4296b83043b7ec55248f74394e2f51ca37f8cb
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-19 09:23:14 -0700

    Add test attributes test to museum test

---
+++ lib/museum.rb
@@ -0,0 +1,6 @@
+class Museum
+
+  def initialize(name)
+    @name = name
+  end
+end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -12,6 +12,11 @@ class MuseumTest < Minitest::Test
   def test_it_exists
     assert_instance_of Museum, @dmns
   end
+
+  def test_it_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+  end
 end
 
 
